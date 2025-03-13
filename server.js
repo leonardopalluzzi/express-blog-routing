63e9c2b91e6759393845d3d05153a3de6be585d8
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-13 14:51:52 +0100

    imoport router into server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const postsRouter = require('./routers/posts');
 
+
+//server on listen
 app.listen(port, () => {
     console.log(`server running on http://localhost:${port}`); 
 });
+
+//middleware
+app.use('/api/v1/posts', postsRouter);
