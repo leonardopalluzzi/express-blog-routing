de12795a938cfb64f8d81181760ee94bd542a424
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-13 14:50:33 +0100

    add CRUD routes

--- routers/posts.js
+++ routers/posts.js
@@ -2,13 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 //index
+router.get('/', (req, res) => {
+    res.send('Posts list');
+});
 
 //show
+router.get('/:slug', (req, res) => {
+    res.send(`Showing post: ${req.params.slug}`);
+});
 
 //store
+router.post('/', (req, res) => {
+    res.send('Upload a new post');
+});
 
 //update
+router.put('/:slug', (req, res) => {
+    res.send(`Update the post: ${req.params.slug}`);
+});
 
 //modify
+router.patch('/:slug', (req, res) => {
+    res.send(`Modify the post: ${req.params.slug}`);
+});
 
 //delete
+router.delete('/:slug', (req, res) => {
+    res.send(`Delete the post: ${req.params.slug}`);
+});
+
+module.exports = router;
