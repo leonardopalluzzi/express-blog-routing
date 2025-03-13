56f358a465d3e2a2d66f58249efd0ee718d47273
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-13 14:46:32 +0100

    add route file base structure

---
+++ routers/posts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+//index
+
+//show
+
+//store
+
+//update
+
+//modify
+
+//delete
--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const app = express();
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`server running on http://localhost:${port}`);
-    
+    console.log(`server running on http://localhost:${port}`); 
 });
